fd22bcbdfa2e6ed1cd584e6fdcb95134ed4a8095
sfc-gh-craghavendra <101361470+sfc-gh-craghavendra@users.noreply.github.com>
2022-05-16 15:55:42 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 
 streamlit.title("My parents' New Health Diner");
+streamlit.header("Breakfast Menu");
+streamlit.text("Omega3 & Blueberry Oatmeal");
+streamlit.text("Hard Bioled Free-range Eggs");
