4078ca2e11c101c2af46c6594e60fe0b2ca98a80
sfc-gh-craghavendra <101361470+sfc-gh-craghavendra@users.noreply.github.com>
2022-05-18 14:56:31 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit
 
 streamlit.header('Breakfast Favorites')
-streamlit.text(🥣 'Omega 3 & Blueberry Oatmeal')
-streamlit.text(🥗 'Kale, Spinach & Rocket Smoothie')
-streamlit.text(🐔'Hard-Boiled Free-Range Egg')
-streamlit.text(🥑🍞'Avocado Toast')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
