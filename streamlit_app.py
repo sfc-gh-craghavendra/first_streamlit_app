3027a3cbc0d12289859cfecfd541795ee1cbcb10
sfc-gh-craghavendra <101361470+sfc-gh-craghavendra@users.noreply.github.com>
2022-06-06 16:12:08 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,8 +20,7 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header('FruityVice Fruit Advice!')
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) # just writes data to screen
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 # take the json version of the response and normalize it
 
