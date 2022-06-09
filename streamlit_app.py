85e1b9dbccaaba1985fa380c8aaa31066795d340
sfc-gh-craghavendra <101361470+sfc-gh-craghavendra@users.noreply.github.com>
2022-06-09 10:33:53 -0400

    Add stop and clean up

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 #import pandas as pd
 #import requests
-import snowflake.connector
+#import snowflake.connector
 from urllib.error import URLError
 
 streamlit.header('Breakfast Favorites')
