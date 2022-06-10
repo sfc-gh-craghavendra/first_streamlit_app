74bac3063a2098c2e75c62679d6b3c8f358152d0
sfc-gh-craghavendra <101361470+sfc-gh-craghavendra@users.noreply.github.com>
2022-06-10 13:36:30 -0400

    Add stop and clean up

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,21 +20,31 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
+# New section to display Fruityvice Advice!
 streamlit.header('FruityVice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?', 'kiwi')
-streamlit.write('The user entered', fruit_choice)
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    if not fruit_choice:
+        streamlit.error("Please select a fruit to get information")
+    else:
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+        fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+        streamlit.dataframe(fruityvice_normalized)
+
+except URLError as e:
+    streamlit.error()
 
 #don't run anything past this
 streamlit.stop()
 
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+
 
 # take the json version of the response and normalize it
 
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+
 #output it to screen as a table
-streamlit.dataframe(fruityvice_normalized)
+
 
 
 
